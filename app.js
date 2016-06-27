9af2a95300ed7d19128678f03c32c84ccf5ba3da
Eray AYDIN <erayaydin2016@gmail.com>
2016-06-27 19:04:23 +0300

    Removed unnecessary upload const

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const passport     = require('passport');
 const expressValidator = require('express-validator');
 const sass             = require('node-sass-middleware');
 const multer           = require('multer');
-const upload           = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
  * Load environment variables such as api keys, passwords
