781fc798b10db9e377123f33c691d5fa9c928e4a
Eray AYDIN <erayaydin2016@gmail.com>
2019-08-06 20:35:36 +0000

    Updates

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ const app = express();
 /**
  * Connect to MongoDB
  */
+2
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.log('MongoDB connection error. Please make sure that MongoDB is running.');
